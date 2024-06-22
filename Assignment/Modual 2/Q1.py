553fcd6a4ae70de947b415c94f2b7034ed539366
karanpatel234 <karanbhalodiya21@gmail.com>
2024-06-22 16:41:20 +0530

    Modual 2

---
+++ Assignment/Modual 2/Q1.py
@@ -0,0 +1,11 @@
+# Write a Python program to check if a number is positive, negative or zero.
+
+
+a=int(input("Enter your number:"))
+
+if a>0:
+    print("your Number is positive.")
+elif a<0:
+    print("your Number is negative.")
+else:
+    print("your Number is ZERO")
